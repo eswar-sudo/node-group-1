d03941ff62b18166835fba6221f78bfaa239befa
eswar-sudo <eswarkumar501@gmail.com>
2025-08-08 14:53:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,9 +68,9 @@ resource "aws_iam_role_policy_attachment" "eks_node_AmazonSSMManagedInstanceCore
 # Node group module main.tf
 resource "aws_eks_node_group" "this" {
   cluster_name    = var.cluster_name
-  subnet_ids      = (var.subnet_ids)
+  subnet_ids      = (var.private_subnet_ids)
   node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
 
    scaling_config {
     desired_size = var.desired_size
